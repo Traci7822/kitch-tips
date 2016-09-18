03790385b3cebf0f151abe7f2791cce2a868c8f4
Traci <Tracir@gmail.com>
2016-09-18 16:30:40 -0600

    built post signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,16 @@ class ApplicationController < Sinatra::Base
 
   post '/signup' do
     if user_params_blank?(params)
-      #look at throwing a flash message here
+      #look at throwing a flash message here "Field cannot be blank, try again"
+      redirect '/signup'
+    elsif
+      User.find_by(:email => params[:email])
+      #Flash message: "User with this email address already exists, try again"
       redirect '/signup'
     else
-      user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      user = User.create(name: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = user.id
+      redirect "/tips"
     end
   end
 
--- app/models/concerns/validify.rb
+++ app/models/concerns/validify.rb
@@ -3,5 +3,6 @@ module Validify
     def user_params_blank?(params)
       if params.has_value?("") || params.has_value?(nil)
       end
+    end
   end
 end

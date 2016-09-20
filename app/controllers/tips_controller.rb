3f539ad8088f0fc3685abdc27c3a571cc31f732c
Traci <Tracir@gmail.com>
2016-09-19 19:02:14 -0600

    functionality to edit a tip implemented

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,7 +1,22 @@
 class TipsController < ApplicationController
 
+  include Validify::InstanceMethods
+
    get '/:id/edit' do
      @tip = Tip.find_by(id: params[:id])
       erb :"/tip/show"
    end
-end
+
+   patch '/tip/:id' do
+     if logged_in?(session)
+       if !params_blank?(params)
+         tip = Tip.find_by(id: params[:id])
+         tip.update(name: params[:name], main_ingredient: params[:main_ingredient], tip: params[:tip])
+         redirect "/user"
+       end
+     else
+       redirect "/login"
+     end
+   end
+
+ end
